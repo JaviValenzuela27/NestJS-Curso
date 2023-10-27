12882a4b3a60d55ef062dd87185d46b0b45d1db4
javier valenzuela <javier.valenzuela@palmerola-airport.com>
2023-10-27 15:03:11 -0600

    comment

--- src/providers/http/http.service.ts
+++ src/providers/http/http.service.ts
@@ -6,13 +6,18 @@ import { ErrorManager } from 'src/utils/error.manager';
 @Injectable()
 export class HttpCustomService {
   constructor(private readonly httpService: HttpService) {}
+
+  //Metodo para consumir api externa
   public async apiFindAll() {
     try {
+      //Busca en la API
       const response = await firstValueFrom(
         this.httpService.get('https://rickandmortyapi.com/api/character'),
       );
+      //Retorna la data obtenida
       return response.data;
     } catch (error) {
+      //Gestiona los errores
       throw ErrorManager.createSignatureError(error.message);
     }
   }
--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,10 +2,14 @@ import { HttpModule } from '@nestjs/axios';
 import { Global, Module } from '@nestjs/common';
 import { HttpCustomService } from './http/http.service';
 
+//Se configura de manera flobal
 @Global()
 @Module({
+  //Se importa el HttpModule de Axios
   imports: [HttpModule],
+  //Se importa el servicio
   providers: [HttpCustomService],
+  //Se exportan ambos para usarlos fuera del modulo
   exports: [HttpModule, HttpCustomService],
 })
 export class ProvidersModule {}
